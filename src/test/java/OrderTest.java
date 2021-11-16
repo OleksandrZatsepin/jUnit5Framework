e771c2f0fdec2babbca96074372c33f78d0a80cd
Oleksandr Zatsepin <alexander.zatsepin@gmail.com>
2021-11-16 17:48:57 +0200

    #2: Added some tests.

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -5,15 +5,15 @@ import org.junit.jupiter.api.Test;
 public class OrderTest extends BaseTest {
 
     @Test
-    void shouldReturnUserByUsername() {
-        final Order order = apiService.getOrderByValidId((long) 1);
-        assert order.getId().equals((long) 1);
-    }
-
-    @Test
     void shouldBeAbleToAddOrder() {
         final Order defaultOrder = Order.getDefaultOrder();
         apiService.addOrder(defaultOrder);
+    }
+
+    @Test
+    void shouldReturnOrderByValidId() {
+        final Order defaultOrder = apiService.getOrderByValidId((long) 1);
+        assert defaultOrder.getId().equals((long) 1);
     }
 
 }
--- src/test/java/PetsTest.java
+++ src/test/java/PetsTest.java
@@ -18,6 +18,20 @@ public class PetsTest extends BaseTest {
     }
 
     @Test
+    void shouldOnlyReturnPetsByPendingStatus() {
+        final List<Pet> response = apiService.findPetsByStatus(PENDING_STATUS);
+        assert response.size() > 0;
+        Assertions.assertThat(response.stream().filter(pet -> !Objects.equals(pet.getStatus(), PENDING_STATUS))).hasSize(0);
+    }
+
+    @Test
+    void shouldOnlyReturnPetsBySoldStatus() {
+        final List<Pet> response = apiService.findPetsByStatus(SOLD_STATUS);
+        assert response.size() > 0;
+        Assertions.assertThat(response.stream().filter(pet -> !Objects.equals(pet.getStatus(), SOLD_STATUS))).hasSize(0);
+    }
+
+    @Test
     void shouldBeAbleToAddPet() {
         final Pet defaultPet = Pet.getDefaultPet();
         apiService.addPet(defaultPet);
