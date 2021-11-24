e3fe2da2af6a41d86606069a87502d712cc2d65a
Oleksandr Zatsepin <alexander.zatsepin@gmail.com>
2021-11-24 12:17:22 +0200

    #2: Changed structure of OrdersTest.

--- src/test/java/OrdersTest.java
+++ src/test/java/OrdersTest.java
@@ -14,57 +14,97 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Order tests:")
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrdersTest extends BaseTest {
 
-    @DisplayName("Add Orders with different statuses")
-    @org.junit.jupiter.api.Order(1)
-    @ParameterizedTest
-    @EnumSource(OrderStatus.class)
-    void shouldReturnOrdersWithAvailableStatus(OrderStatus orderStatus) throws ApiException {
-        Order response = apiService.addOrder(Order.getDefaultOrder(orderStatus));
-        assertThat(response).matches(order -> order.getStatus().equals(response.getStatus()));
-        System.out.println("Order with Id " + response.getId().toString() + " for pet with petId " + response.getPetId().toString() + " was created successfully");
+    @Nested
+    @DisplayName("Endpoint: /store/order, method: POST")
+    class OrderTestsForPostMethod {
+        @Nested
+        @DisplayName("Positive tests")
+        class PositiveOrderTestsForPostMethod {
+            @DisplayName("Add Orders with different statuses")
+            @ParameterizedTest
+            @EnumSource(OrderStatus.class)
+            void shouldReturnOrdersWithAvailableStatus(OrderStatus orderStatus) throws ApiException {
+                Order response = apiService.addOrder(Order.getDefaultOrder(orderStatus));
+                assertThat(response).matches(order -> order.getStatus().equals(response.getStatus()));
+                System.out.println("Order with Id " + response.getId().toString()
+                        + " for pet with petId " + response.getPetId().toString()
+                        + " was created successfully");
+            }
+
+            @DisplayName("Add Orders with test orderId >= 1 and <= 10")
+            @ParameterizedTest
+            @ValueSource(longs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+            void shouldReturnOrdersWithTestId(Long orderId) throws ApiException {
+                Order testOrder = Order.getDefaultOrder();
+                testOrder.setId(orderId);
+                Order response = apiService.addOrder(testOrder);
+                assertThat(response).matches(order -> order.getId().equals(response.getId()));
+                System.out.println("Order with Id " + response.getId().toString()
+                        + " was created successfully");
+            }
+        }
+
+        @Nested
+        @DisplayName("Negative tests")
+        class NegativeOrderTestsForPostMethod {
+
+        }
     }
 
-    @DisplayName("Add Orders with test orderId >= 1 and <= 10")
-    @org.junit.jupiter.api.Order(2)
-    @ParameterizedTest
-    @ValueSource(longs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-    void shouldReturnOrdersWithTestId(Long orderId) throws ApiException {
-        Order testOrder = Order.getDefaultOrder();
-        testOrder.setId(orderId);
-        Order response = apiService.addOrder(testOrder);
-        assertThat(response).matches(order -> order.getId().equals(response.getId()));
-        System.out.println("Order with Id " + response.getId().toString() + " was created successfully");
+    @Nested
+    @DisplayName("Endpoint: /store/order/{orderId}, method: GET")
+
+    class OrderTestsForGetMethod {
+        @Nested
+        @DisplayName("Positive tests")
+        class PositiveOrderTestsForGetMethod {
+            @DisplayName("Find Test Orders with orderId >= 1 and <= 10")
+            @ParameterizedTest
+            @ValueSource(longs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+            void shouldFindOrdersWithTestId(Long orderId) throws ApiException {
+                Order response = apiService.findOrderById(orderId);
+                assertThat(response).matches(order -> order.getId().equals(response.getId()));
+                System.out.println("Order with Id " + response.getId().toString()
+                        + " was found successfully");
+            }
+        }
+
+        @Nested
+        @DisplayName("Negative tests")
+        class NegativeOrderTestsForPostMethod {
+        }
     }
 
-    @DisplayName("Find Test Orders with orderId >= 1 and <= 10")
-    @org.junit.jupiter.api.Order(3)
-    @ParameterizedTest
-    @ValueSource(longs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-    void shouldFindOrdersWithTestId(Long orderId) throws ApiException {
-        Order response = apiService.findOrderById(orderId);
-        assertThat(response).matches(order -> order.getId().equals(response.getId()));
-        System.out.println("Order with Id " + response.getId().toString() + " was found successfully");
-    }
+    @Nested
+    @DisplayName("Endpoint: /store/order/{orderId}, method: DELETE")
+    class OrderTestsForDeleteMethod {
+        @Nested
+        @DisplayName("Positive tests")
+        class PositiveOrderTestsForDeleteMethod {
+            @DisplayName("Remove Test Orders with orderId >= 1 and <= 10")
+            @ParameterizedTest
+            @ValueSource(longs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+            void shouldDeleteOrdersWithTestId(Long orderId) throws ApiException {
+                ApiResponse response = apiService.removeOrderById(orderId);
+                assertThat(response.getCode()).isEqualTo(STATUS_CODE_OK);
+                System.out.println("Order with Id " + response.getMessage()
+                        + " was removed successfully");
+            }
+        }
 
-    @DisplayName("Remove Test Orders with orderId >= 1 and <= 10")
-    @org.junit.jupiter.api.Order(4)
-    @ParameterizedTest
-    @ValueSource(longs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-    void shouldDeleteOrdersWithTestId(Long orderId) throws ApiException {
-        ApiResponse response = apiService.removeOrderById(orderId);
-        assertThat(response.getCode()).isEqualTo(STATUS_CODE_OK);
-        System.out.println("Order with Id " + response.getMessage() + " was removed successfully");
-    }
-
-    @DisplayName("Throws exception, when try to remove random Order")
-    @org.junit.jupiter.api.Order(5)
-    @Test
-    void shouldThrowExceptionWhenTryRemoveRandomOrder() {
-        Order order = Order.getDefaultOrder();
-        assertThrows(ApiException.class, () -> apiService.removeOrderById(order.getId()), "throws when Order not found");
-        System.out.println("Test Order with orderId " + order.getId() + " was not found");
+        @Nested
+        @DisplayName("Negative tests")
+        class NegativeOrderTestsForDeleteMethod {
+            @DisplayName("Throws exception, when try to remove random Order")
+            @Test
+            void shouldThrowExceptionWhenTryRemoveRandomOrder() {
+                Order order = Order.getDefaultOrder();
+                assertThrows(ApiException.class, () -> apiService.removeOrderById(order.getId()), "throws when Order not found");
+                System.out.println("Test Order with orderId " + order.getId()
+                        + " was not found");
+            }
+        }
     }
 }
